278e90e452b150a09cb482aaf6bdd1cf7c90639d
Alexandra Prodan <prodan.sasha@gmail.com>
2016-01-27 17:01:08 -0600

    Final Refactoring

--- atbash_cipher/atbash_cipher.rb
+++ atbash_cipher/atbash_cipher.rb
@@ -3,19 +3,15 @@ class Atbash
   NUMBERS = Hash[('0'..'9').to_a.zip(('0'..'9').to_a)]
   CIPHER = LETTERS.merge(NUMBERS)
 
-  def self.encode(word)
-    if word.size <= 5
-      CIPHER.values_at(*word.downcase.gsub(/\W/, '').chars).join
-    elsif word.size > 5
-      edit(word)
-    end
+  def self.encode(string)
+    string.size <= 5 ? encode_word(string).join : edit(string)
   end
 
-  def self.edit(word)
-    arr = CIPHER.values_at(*word.downcase.gsub(/\W/, '').chars)
-    arr.each_slice(5).map do |s|
-      s.join('')
-    end
-    .join(' ')
+  def self.edit(string)
+    encode_word(string).each_slice(5).map { |s| s.join('') }.join(' ')
+  end
+
+  def self.encode_word(string)
+    CIPHER.values_at(*string.downcase.gsub(/\W/, '').chars)
   end
 end
