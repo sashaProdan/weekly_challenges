0f244ef7ac82dd9d61dbca5e0f61e07f5469976b
Alexandra Prodan <prodan.sasha@gmail.com>
2016-06-04 18:42:17 -0500

    Refactor word_count.rb

--- word_count/word_count.rb
+++ word_count/word_count.rb
@@ -4,21 +4,14 @@ class Phrase
   end
 
   def word_count
-    word_array.inject({}) do |counts, (word, count)|
-      counts[word] = count
-      counts
+    word_split.each_with_object(Hash.new(0)) do |word, hash|
+      hash[word] += 1
     end
   end
  
   private
 
   def word_split
-    @phrase.scan(/\b[\w']+\b/).flatten.map(&:downcase)
-  end
-
-  def word_array
-    word_split.map do |word|
-      [word, word_split.count(word)]
-    end.uniq
+    @phrase.scan(/\b[\w']+\b/).map(&:downcase)
   end
 end
