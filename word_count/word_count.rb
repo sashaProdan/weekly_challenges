48f0f0ca4de8b4af90bdc1bafc2278d575441a8b
Alexandra Prodan <prodan.sasha@gmail.com>
2016-06-07 14:29:23 -0500

    Final refactoring of word_count.rb

--- word_count/word_count.rb
+++ word_count/word_count.rb
@@ -1,17 +1,12 @@
 class Phrase
   def initialize(string)
-    @phrase = string
+    @phrase = string.scan(/\b[\w']+\b/).map(&:downcase)
   end
 
   def word_count
-    word_split.each_with_object(Hash.new(0)) do |word, hash|
+    @phrase.each_with_object(Hash.new(0)) do |word, hash|
       hash[word] += 1
     end
   end
- 
-  private
-
-  def word_split
-    @phrase.scan(/\b[\w']+\b/).map(&:downcase)
-  end
 end
+
