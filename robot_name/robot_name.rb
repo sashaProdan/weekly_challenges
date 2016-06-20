31f1f0a7395a23bdd05ba2bd15ad0258fb6115d6
Alexandra Prodan <prodan.sasha@gmail.com>
2016-06-20 18:32:29 -0500

    initial solution

---
+++ robot_name/robot_name.rb
@@ -0,0 +1,35 @@
+class Robot
+  attr_accessor :name
+  @@robots = []
+  def initialize
+    @name = self.random_name
+    @@robots << self.name
+  end
+
+  def random_name
+    name = ''
+    2.times do
+      name  << ('a'..'z').to_a.sample
+    end
+    3.times do
+      name  << (1..9).to_a.sample.to_s
+    end
+    no_duplicate(name.upcase)
+  end
+
+  def reset
+    @name = self.random_name
+  end
+
+  def no_duplicate(name)
+    if @@robots.include? name
+      reset
+    else
+      name
+    end
+  end
+
+end
+
+
+
