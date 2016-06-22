b0b472cb4dd5d13685346dd490affd6ca430e048
Alexandra Prodan <prodan.sasha@gmail.com>
2016-06-21 20:57:34 -0500

    refactor class methods

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,15 +1,17 @@
 class Hamming
-  def self.compute(strand1, strand2)
-    Hamming.equal_size(strand1, strand2)
-    sequence = strand1.chars.zip strand2.chars
-    distance = 0
-    sequence.each do |arr|
-      distance += 1 if arr[0] != arr[1]
+  class << self
+    def compute(strand1, strand2)
+      Hamming.equal_size(strand1, strand2)
+      sequence = strand1.chars.zip strand2.chars
+      distance = 0
+      sequence.each do |arr|
+        distance += 1 if arr[0] != arr[1]
+      end
+      distance
     end
-    distance
-  end
 
-  def self.equal_size(str1, str2)
-    raise ArgumentError if str1.size != str2.size
+    def equal_size(str1, str2)
+      raise ArgumentError if str1.size != str2.size
+    end
   end
 end
