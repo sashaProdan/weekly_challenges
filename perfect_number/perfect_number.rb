d41fdf187aa219d309cf4b8516c5b3b270a0d9b9
Alexandra Prodan <prodan.sasha@gmail.com>
2016-07-04 16:17:31 -0500

    final refactor

--- perfect_number/perfect_number.rb
+++ perfect_number/perfect_number.rb
@@ -1,8 +1,6 @@
-class RuntimeError < StandardError; end
-
 module PerfectNumber
   def self.classify(num)
-    raise RuntimeError if num <= 0
+    raise RuntimeError, "Please enter a positive number!" if num < 0
 
     if prime?(num) || sum_of_factors(num) < num
       'deficient'
@@ -17,7 +15,11 @@ module PerfectNumber
     (2...num).all? { |divisor| num % divisor != 0 }
   end
 
+  def self.factors(num)
+    (1...num).select {|divisor| num % divisor == 0}
+  end
+
   def self.sum_of_factors(num)
-    (1...num).select {|divisor| num % divisor == 0}.inject(0, :+)
+    factors(num).inject(:+)
   end
 end
