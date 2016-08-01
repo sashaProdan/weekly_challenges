dfe6cf056730d4b5bd64c95524865e484999f213
Alexandra Prodan <prodan.sasha@gmail.com>
2016-08-01 17:53:10 -0500

    Refactor

--- atbash_cipher/atbash_cipher_second.rb
+++ atbash_cipher/atbash_cipher_second.rb
@@ -2,23 +2,19 @@ class Atbash
   CIPHER = Hash[('a'..'z').to_a.zip(('a'..'z').to_a.reverse)]
 
   def self.encode(str)
-    encode_word(str).each_slice(5)
-                    .map {|a| a.join}
-                    .join(' ')
+    initial_encode(str).each_slice(5)
+                       .map(&:join)
+                       .join(' ')
   end
 
   def self.format(str)
     str.downcase.scan(/[\w+]/)
   end
 
-  def self.encode_word(str)
+  def self.initial_encode(str)
     encoded = []
     format(str).each do |char|
-      if CIPHER.has_key? char
-        encoded << CIPHER[char]
-      else
-        encoded << char
-      end
+      CIPHER.has_key?(char) ? (encoded << CIPHER[char]) : (encoded << char)
     end
     encoded
   end
